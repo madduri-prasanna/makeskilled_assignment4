697f68999d8243d3c5fe2e6372fa01d9cef22b73
madduri-prasanna <madduriprasanna8@gmail.com>
2023-05-22 13:16:15 +0530

    changed from hello world to palindrome

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-msg="Hello world"
-print(msg)
+# function which return reverse of a string
+  
+def isPalindrome(s):
+    return s == s[::-1]
+  
+  
+# Driver code
+s = "malayalam"
+ans = isPalindrome(s)
+  
+if ans:
+    print("Yes")
+else:
+    print("No")
